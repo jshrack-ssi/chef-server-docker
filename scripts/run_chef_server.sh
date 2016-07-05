19d9e20244e4b35c7787fee8769d7babb32aa0db
Jason Shrack <jshrack@servicesource.com>
2016-07-04 17:52:29 -0700

    Add --accept-license flag to chef manage & reporting modules

--- scripts/run_chef_server.sh
+++ scripts/run_chef_server.sh
@@ -12,21 +12,19 @@ fi
 
 apt-get update
 
-chef-server-ctl install opscode-manage
-
 rm -f /var/lib/apt/lists/partial/packagecloud.io_chef_stable_ubuntu_dists_lucid_main_*
 
-chef-server-ctl install opscode-manage
+chef-server-ctl install chef-manage
 
-/opt/opscode-manage/embedded/bin/runsvdir-start &
+/opt/chef-manage/embedded/bin/runsvdir-start & 
 
-chef-server-ctl reconfigure && opscode-manage-ctl reconfigure
+chef-server-ctl reconfigure && chef-manage-ctl reconfigure --accept-license || /bin/bash
 
-chef-server-ctl install opscode-reporting
+chef-server-ctl install chef-reporting
 
-/opt/opscode-reporting/embedded/bin/runsvdir-start &
+/opt/chef-reporting/embedded/bin/runsvdir-start & 
 
-chef-server-ctl reconfigure && opscode-reporting-ctl reconfigure
+chef-server-ctl reconfigure && chef-reporting-ctl reconfigure --accept-license || /bin/bash
 
 chef-server-ctl user-create jshrack Jason Shrack jshrack@gmail.com mT1MIVdheo20 --filename /root/chef_server_admin.pem && \
 chef-server-ctl org-create killbox1a "KillBox1A" --association_user jshrack --filename /root/killbox1a-validator.pem
